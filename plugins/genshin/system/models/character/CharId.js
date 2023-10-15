9cae00bfee67cdd38a3fcf70de455e3186602298
饼干 <2562248115@qq.com>
2023-10-15 22:13:42 +0800

    更

--- plugins/genshin/system/models/character/CharId.js
+++ plugins/genshin/system/models/character/CharId.js
@@ -2,7 +2,7 @@
  * 角色别名及角色ID相关
  * */
 import lodash from 'lodash'
-import { Data, Format } from '#miao'
+import { Data, Format } from '#yunzai'
 import { charPosIdx } from './CharMeta.js'
 import { aliasMap as aliasMapSR } from '../../resources/meta-sr/character/meta.js'
 
