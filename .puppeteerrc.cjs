d264f95dbab10768423fdb9b180744927438e0f0
Yummy-cookie <2562248115@qq.com>
2023-11-18 02:41:05 +0800

    fix

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,33 +1,47 @@
-const os = require("os");
-const { existsSync } = require("fs");
-const arch = os.arch();
-let skipDownload = false;
-let executablePath;
-//win32 存在 Edge 优先选择
-if (process.platform == "win32") {
-  if (
-    existsSync("C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe")
-  ) {
-    skipDownload = true;
-    executablePath =
-      "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe";
-  }
-} else if (process.platform == "linux") {
-  //如果arm64架构跳过下载
-  if (arch == "arm64" || arch == "aarch64") {
-    skipDownload = true;
-  }
-  //不管什么架构,如果存在配置则跳过下载,且配置路径
-  if (existsSync("/usr/bin/chromium")) {
-    skipDownload = true;
-    executablePath = "/usr/bin/chromium";
-  }
+const os = require('os')
+const { existsSync } = require('fs')
+const { execSync } = require('child_process')
+const arch = os.arch()
+
+let skipDownload = false
+let executablePath
+
+if (process.platform === 'linux' || process.platform === 'android')
+  for (const item of [
+    "chromium",
+    "chromium-browser",
+    "chrome",
+  ]) try {
+    const chromiumPath = execSync(`command -v ${item}`).toString().trim()
+    if (chromiumPath && existsSync(chromiumPath)) {
+      executablePath = chromiumPath
+      break
+    }
+  } catch (err) {}
+
+// macOS
+if (process.platform === 'darwin') for (const item of [
+  '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge',
+]) if (existsSync(item)) {
+  executablePath = item
+  break
 }
 
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  skipDownload,
-  executablePath,
-};
+if (!executablePath) for (const item of [
+  '/usr/bin/chromium',
+  '/usr/bin/chromium-browser',
+  '/usr/bin/chrome',
+  'C:/Program Files/Google/Chrome/Application/chrome.exe',
+  'C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe',
+]) if (existsSync(item)) {
+  executablePath = item
+  break
+}
+
+if (executablePath || arch === 'arm64' || arch === 'aarch64') {
+  (typeof logger == 'object' ? logger : console).info(`[Chromium] ${executablePath}`)
+  skipDownload = true
+}
+
+module.exports = { skipDownload, executablePath }
