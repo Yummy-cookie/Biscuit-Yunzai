b55a640a8b51d500c536998cbaba0d02ad0d2fb9
Yummy-cookie <2562248115@qq.com>
2023-12-17 16:31:43 +0800

    fix

--- plugins/genshin/model/abyss.js
+++ plugins/genshin/model/abyss.js
@@ -2,7 +2,6 @@ import moment from 'moment'
 import lodash from 'lodash'
 import base from './base.js'
 import MysInfo from './mys/mysInfo.js'
-import gsCfg from './gsCfg.js'
 
 export default class Abyss extends base {
   constructor (e) {
@@ -82,6 +81,7 @@ export default class Abyss extends base {
           }
         ]
       }
+
       rankData[val] = {
         num: data[`${val}_rank`][0].value,
         name: gsCfg.roleIdToName(data[`${val}_rank`][0].avatar_id)
@@ -204,10 +204,9 @@ export default class Abyss extends base {
 
       for (let i in val.battles) {
         for (let j in val.battles[i].avatars) {
-          val.battles[i].avatars[j].name = roleArr[val.battles[i].avatars[j].id].name
-
-          val.battles[i].avatars[j].name = gsCfg.roleIdToName(val.battles[i].avatars[j].id)
-
+          let char = Character.get(val.battles[i].avatars[j].id)
+          val.battles[i].avatars[j].name = char.abbr
+          val.battles[i].avatars[j].icon = char.face
           val.battles[i].avatars[j].life = roleArr[val.battles[i].avatars[j].id].actived_constellation_num
         }
       }
